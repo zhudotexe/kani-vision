ab73e0c1bfc20deace9e9f098835c1ffe1ee97d8
Andrew Zhu <me@andrew-zhu.com>
2023-10-23 13:40:10 -0400

    feat: basic image part implementations

--- kani/ext/vision/parts.py
+++ kani/ext/vision/parts.py
@@ -1,44 +1,81 @@
+import abc
+import base64
+import pathlib
+from io import BytesIO
+
 from PIL import Image
 
 from kani import MessagePart
 from kani.utils.typing import PathLike
 
 
-class ImagePart(MessagePart):
+class ImagePart(MessagePart, abc.ABC):
     # constructors
-    @classmethod
-    def from_path(cls, fp: PathLike):
-        pass
+    @staticmethod
+    def from_path(fp: PathLike):
+        """Load an image from a path on the local filesystem."""
+        return FileImagePart(path=fp)
 
-    @classmethod
-    def from_bytes(cls, data: bytes):
-        pass
+    @staticmethod
+    def from_bytes(data: bytes):
+        """Load an image from binary data in memory."""
+        return BytesImagePart(data=data)
 
-    @classmethod
-    def from_image(cls, image: Image):
-        pass
+    @staticmethod
+    def from_image(image: Image):
+        """Create an image part from an existing :class:`PIL.Image`."""
+        return PillowImagePart(image=image)
 
     # props
     @property
     def image(self) -> Image:
-        return
+        """Get a :class:`PIL.Image` representing the image."""
+        raise NotImplementedError
 
     @property
     def bytes(self) -> bytes:
-        return
+        """The binary image data."""
+        io = BytesIO()
+        self.image.save(io, format="PNG")
+        return io.getvalue()
 
     @property
     def b64(self) -> str:
-        return
+        """The binary image data encoded in a base64 string.
+
+        Note that this is *not* a web-suitable ``data:image/...`` string; just the raw binary of the image.
+        """
+        return base64.b64encode(self.bytes).decode()
 
 
 class FileImagePart(ImagePart):
-    pass
+    path: pathlib.Path
+
+    @property
+    def image(self):
+        return Image.open(self.path)
+
+    @property
+    def bytes(self):
+        with open(self.path, "rb") as f:
+            return f.read()
 
 
 class BytesImagePart(ImagePart):
-    pass
+    data: bytes
+
+    @property
+    def image(self):
+        return Image.open(BytesIO(self.data))
+
+    @property
+    def bytes(self):
+        return self.bytes
 
 
 class PillowImagePart(ImagePart):
-    pass
+    image: Image
+
+    @property
+    def image(self):
+        return self.image
