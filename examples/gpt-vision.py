8c065962c68ed26b7396466d563dd3bb7fff6e4c
Andrew Zhu <me@andrew-zhu.com>
2023-10-25 16:38:20 -0400

    fix: debug image in terminal

--- examples/gpt-vision.py
+++ examples/gpt-vision.py
@@ -11,4 +11,5 @@ ai = Kani(engine)
 
 if __name__ == "__main__":
     # use `!path/to/file.png` to provide an image to the engine, e.g. `Please describe this image: !kani-logo.png`
+    # or use a URL: `Please describe this image: !https://example.com/image.png`
     chat_in_terminal_vision(ai)
--- kani/ext/vision/cli.py
+++ kani/ext/vision/cli.py
@@ -19,12 +19,16 @@ from .parts import ImagePart
 _has_ascii = importlib.util.find_spec("ascii_magic") is not None
 _is_notebook = "ipykernel" in sys.modules
 
+log = logging.getLogger(__name__)
+
 # it's time to get s p i c y
 # please don't do this in prod, this is just for a dev helper
 # @formatter:off
 # https://gist.github.com/gruber/8891611
 WEB_REGEX = r"""(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\b/?(?!@)))"""  # noqa: E501
-BANG_IMAGE_RE = re.compile(rf"!(?P<path>/?(\S+?/)*([^/\s]+\.[^/\s]+))|(?P<path_quot>\"/?(.+?/)*([^/]+\.[^/\s]+)\")|(?P<url>{WEB_REGEX})")  # noqa: E501
+BANG_IMAGE_RE = re.compile(
+    rf"!(?P<url>{WEB_REGEX})|(?P<path>/?(\S+?/)*([^/\s]+\.[^/\s]+))|(?P<path_quot>\"/?(.+?/)*([^/]+\.[^/\s]+)\")"
+)
 # @formatter:on
 
 
@@ -48,14 +52,17 @@ async def parts_from_cli_query(query: str) -> list[MessagePartType]:
         # if a url:
         else:
             # download the image to a named temp file and return that path
+            url = image_match["url"]
+            log.debug(f"Downloading image url: {url}")
             async with aiohttp.ClientSession() as session:
-                async with session.get(image_match["url"]) as resp:
+                async with session.get(url) as resp:
                     with tempfile.NamedTemporaryFile(delete=False) as f:
                         async for chunk in resp.content.iter_chunked(4096):
                             f.write(chunk)
-                        fp = f.name
+                        fp = pathlib.Path(f.name)
 
         # if not, push the string to parts
+        log.debug(f"Found image path: {fp}")
         if not (fp.exists() and fp.is_file()):
             warnings.warn(f"The given image path ({fp}) either does not exist or is not a valid file.")
             query_parts.append(image_match[0])
